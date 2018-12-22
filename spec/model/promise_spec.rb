f17ad1266c6757378ffb69ab28ef0912a9809a91
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-21 17:53:45 -0700

    Resolve bug in promise model test

--- spec/model/promise_spec.rb
+++ spec/model/promise_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
-
-describe Promise do
+describe Promise, type: :model do
   describe 'validations' do
     it {should validate_presence_of :maker_name}
     it {should validate_presence_of :content}
     it {should validate_presence_of :status}
-  end 
+  end
 end
