deab40dd1c1cce8c235222de179f154c3c509f1e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-21 20:08:24 -0700

    Add create functionality & make tests pass

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -3,8 +3,17 @@ class PromisesController < ApplicationController
     @promises = Promise.all
   end
 
-
   def new
     @promise = Promise.new
+  end
+
+  def create
+    @promise = Promise.create(promise_params)
+    redirect_to promises_path
+  end
+
+  private
+  def promise_params
+    params.require(:promise).permit(:maker_name, :content)
   end
 end
---
+++ spec/features/user_creates_new_promise_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'promise creation' do
+  describe 'when a user fills out a form & clicks submit' do
+    it 'allows a user to create a promise' do
+      visit new_promise_path
+
+      fill_in :promise_maker_name, with: "Finn"
+      fill_in :promise_content, with: "Build a spaceship"
+
+      click_on "Create Promise"
+
+      new_promise = Promise.last
+
+      expect(new_promise.maker_name).to eq("Finn")
+      expect(current_path).to eq(promises_path)
+      expect(page).to have_content("Finn")
+      expect(page).to have_content("Build a spaceship")
+    end
+  end
+end
