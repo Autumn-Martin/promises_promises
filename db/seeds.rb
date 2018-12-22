6d76fff8c918398803501ac60b3e1cbd3b9b5fab
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-21 18:21:54 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Promise.create!(content: "Solve mysteries",
+                           maker_name: "Dipper")
+Promise.create!(content: "Find a unicorn",
+                           maker_name: "Mabel")
+Promise.create!(content: "Pay back $5",
+                           maker_name: "Grunkle Stan")
+Promise.create!(content: "Become a hero",
+                           maker_name: "Soos")
+Promise.create!(content: "Be cool and save the day",
+                           maker_name: "Wendy")
+puts "All #{Promise.all.count} promises created!"
